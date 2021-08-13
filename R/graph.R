cf9974e9da16b3583642c8c2644213bf862de078
Meera Patel <patelmeeray@gmail.com>
2021-08-13 00:03:31 -0400

    deploy pkg using glitter

--- R/graph.R
+++ R/graph.R
@@ -162,7 +162,19 @@ filter_graph <-
 
     }
 
-    omop_graph
+    # If the filter is applied to a node, then
+    # then filtering edges based on `from` and `to`
+
+    omop_graph@src@edges@data <-
+      dplyr::bind_rows(
+        omop_graph@src@edges@data %>%
+          dplyr::filter(omop_graph@src@edges@data$from %in% omop_graph@src@nodes@data@id),
+        omop_graph@src@edges@data %>%
+          dplyr::filter(omop_graph@src@edges@data$to %in% omop_graph@src@nodes@data@id)) %>%
+      dplyr::distinct()
 
 
+    construct_graph(
+      nodes_and_edges = omop_graph@src
+    )
   }
