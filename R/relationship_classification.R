767933946e7d79e8522d3626981560bfe0a14b0a
Meera Patel <patelmeeray@gmail.com>
2021-08-21 20:38:47 -0400

    add key retrieval from vocabulary table

--- R/relationship_classification.R
+++ R/relationship_classification.R
@@ -4,4 +4,6 @@ omop_relationship_id_classification <-
   list(Mapping =
          c('Mapped from', 'Maps to'),
        Taxonomy =
-         c('Subsumes', 'Is a'))
+         c('Subsumes', 'Is a'),
+       Update =
+         c("Concept replaced by", "Concept replaces"))
