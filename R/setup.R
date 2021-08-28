4623cec51d6fe2dbc81fad16613645f75f0b2cee
Meera Patel <patelmeeray@gmail.com>
2021-08-28 13:47:59 -0400

    deploy pkg using glitter

--- R/setup.R
+++ R/setup.R
@@ -21,7 +21,7 @@ setup_chariotViz <-
            version_key) {
 
 
-
+    cli::cli_h1("omop.relationships")
     invisible(fetch_omop_relationships(conn = conn,
                              conn_fun = conn_fun,
                              schema = schema,
@@ -29,7 +29,7 @@ setup_chariotViz <-
                              render_sql = render_sql,
                              version_key = version_key))
 
-
+    cli::cli_h1("complete.omop.relationships")
     invisible(fetch_complete_omop_relationships(conn = conn,
                                       conn_fun = conn_fun,
                                       schema = schema,
@@ -37,6 +37,7 @@ setup_chariotViz <-
                                       render_sql = render_sql,
                                       version_key = version_key))
 
+    cli::cli_h1("omop.ancestors")
     invisible(
     fetch_omop_ancestors(conn = conn,
                          conn_fun = conn_fun,
